a678deae371e94004cfc85bdc583110acea5adc3
Monte-Kristo <83087001+RATTER95292@users.noreply.github.com>
2022-10-22 18:41:25 +0300

    Update main.py
    Еще один ванин коммит

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
+import PIL
+import os.path
+import glob
 import numpy as np
+from PIL import Image
 
 #фунция что бы открыть папку
 print(os.listdir("C:\2 ИСХОДНЫЕ СНИМКИ для полигонов\96 kv\RGB"))
@@ -7,3 +11,20 @@ print(os.listdir("C:\2 ИСХОДНЫЕ СНИМКИ для полигонов\9
 with print(os.listdir("C:\2 ИСХОДНЫЕ СНИМКИ для полигонов\96 kv\RGB")) as file:
 #массив
 array = [row.strip() for row in file]
+#обрезка фото
+# Обрезает исходное изображение до 20 * 20 пикселей
+def convertjpg(jpgfile,outdir,width=1920,height=1240):
+    img=Image.open(jpgfile)
+    try:
+        new_img=img.resize((width,height),Image.BILINEAR)   
+        new_img.save(os.path.join(outdir,os.path.basename(jpgfile)))
+    except Exception as e:
+        print(e)
+
+# Равномерно разрезать весь формат PNG под этой папкой
+for jpgfile in glob.glob('/home/luke/INRIAPerson/neg/*.png'):
+
+# создаем папку вывода и выводим туда фото
+if not os.path.isdir("foto"):
+     os.mkdir("foto")
+      convertjpg(jpgfile,'foto')
