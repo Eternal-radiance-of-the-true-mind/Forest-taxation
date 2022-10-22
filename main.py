469b81b4d53acccd2b540ea4b048336ca7d7a4eb
Monte-Kristo <83087001+RATTER95292@users.noreply.github.com>
2022-10-22 17:56:18 +0300

    Update main.py
    Ванин комит

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
+import numpy as np
 
 #фунция что бы открыть папку
 print(os.listdir("C:\2 ИСХОДНЫЕ СНИМКИ для полигонов\96 kv\RGB"))
-
+#фунция что бы открыть папку и считывает сколько там файлов
+with print(os.listdir("C:\2 ИСХОДНЫЕ СНИМКИ для полигонов\96 kv\RGB")) as file:
+#массив
+array = [row.strip() for row in file]
